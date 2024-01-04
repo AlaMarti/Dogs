bfdd0ec1eb20a0eb9e3c6809703b71f7dddc20bf
AlaMarti <rodrigoalanmartinez@gmail.com>
2024-01-04 00:48:40 -0300

    Modificando la searchbar para hacerlo un poco mas dinamico

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import styles from './navbar.module.css'
 import Searchbar from '../searchbar/searchbar'
 
-const navbar = () => {
+const Navbar = () => {
+
+  const [searchVisible, setSearchVisible] = useState(false)
+
+  const toggleSearch = () => {
+    setSearchVisible(!searchVisible);
+  };
+
+
+
+
+
+
   return (
     <div className={styles.navbarCont}>
       <div className={styles.links}>
         <NavLink>
-          <Link to={"/home"} className={styles.home}> Home </Link>
-          <Link to={"/create"} className={styles.create}> Create </Link>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+          <Link to={"/home"} className={styles.home}> <i class="fa-solid fa-house"></i></Link>
+          <Link to={"/create"} className={styles.create}> <i class="fa-solid fa-dog"></i></Link>
+
+
         </NavLink>
       </div>
+
       <div className={styles.barra}>
-        <Searchbar/>
+        <div onClick={toggleSearch}>
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+
+        {searchVisible && (
+          <div className={styles.searchBar }>
+            <Searchbar></Searchbar>
+          </div>
+        )}
       </div>
-        
+
+
     </div>
+
   )
 }
 
-export default navbar
+export default Navbar
